66991f6343ba9635a7cf697def61bb2d77061288
Mohamad Houjeiry <mohamadfaroukhoujeiry@gmail.com>
2025-08-12 10:22:07 +0300

    Athentication, Signup & Login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+       $validatedData = $request->validate([
+           'name' => 'required|string|max:255',
+           'email' => 'required|string|email|max:255|unique:users',
+           'password' => 'required|string|min:8|confirmed',
+       ]);
+
+       $user = User::create([
+           'name' => $validatedData['name'],
+           'email' => $validatedData['email'],
+           'password' => Hash::make($validatedData['password']),
+       ]);
+       $token = $user->createToken('token')->plainTextToken;
+
+       return response()->json(['message' => 'User registered successfully', 'user' => $user, 'token' => $token], 201);
+    }
+public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+      if (!Auth::attempt($credentials)) {
+    return response()->json(['message' => 'email or password is incorrect'], 401);
+}
+
+$user = User::where('email', $credentials['email'])->first();
+$token = $user->createToken('token')->plainTextToken;
+
+
+        return response()->json(['message' => 'Login successful', 'user' => $user, 'token' => $token]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+use App\Http\Controllers\AuthController;
+
+Route::post("register", [AuthController::class, "register"]);
+
+Route::get("login",[AuthController::class,"login"]);
